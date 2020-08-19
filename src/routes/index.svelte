852a601c49007751abbeb170bd434d5c990c7eea
자유해결사 <khjde1207@knou.ac.kr>
2020-08-19 15:32:14 +0900

    키보드 테스트

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,34 +6,67 @@
 	import axios from 'axios'
 
 	let state = {};
-	let subscribers = [];
+	// let subscribers = [];
 
-	let Messages = {};
-	let Constants = {};
+	let SpiceConn={};
+	// let Messages = {};
+	// let Constants = {};
 
 	const { preloading, page, session } = stores();
 	let SpiceHtml5 = null;
 	let domspice; 
 
 
-	onDestroy(() => {
-		subscribers.forEach((unsubscribe) => unsubscribe());
-	});
+	// onDestroy(() => {
+	// 	subscribers.forEach((unsubscribe) => unsubscribe());
+	// });
 	async function asdf(){
-		let keydown = new KeyboardEvent('keydown',{type: "keydown", code: "KeyW" , key: "w", keyCode: 87})
-		var key = new Messages.SpiceMsgcKeyDown(keydown)
-		var msg = new Messages.SpiceMiniData();
-		msg.build_msg(Constants.SPICE_MSGC_INPUTS_KEY_DOWN, key); 
-		// console.log(msg, Constants)
-		// state.inputs.send_msg(msg);
-		console.log(state.inputs.state)
-		let keyup = new KeyboardEvent('keyup',{type: "keyup" ,code: "KeyW" , key: "w", keyCode: 87})
-		var key = new Messages.SpiceMsgcKeyDown(keyup)
-		var msg = new Messages.SpiceMiniData();
-		msg.build_msg(Constants.SPICE_MSGC_INPUTS_KEY_UP, key);
-		// check_and_update_modifiers(keydown, key.code, this.sc);
-		state.inputs.send_msg(msg);
-		console.log(msg)
+		// -128+16
+		// idx = 16 ;
+
+		for (let  i = 16 ; i <= 25 ; i++){
+			let ss = new Int8Array([101, 0, 4, 0, 0, 0, i, 0, 0, 0])
+			state.inputs.ws.send(ss.buffer)
+			setTimeout(() => {
+				let ss = new Int8Array([102, 0, 4, 0, 0, 0,  -128+i, 0, 0, 0])
+				state.inputs.ws.send(ss.buffer)
+		}, 100)
+		}
+		// console.log(state.inputs.ws)
+		// return 
+		// console.log(state.inputs.ws);
+		// let ss = new Int8Array([101, 0, 4, 0, 0, 0, 16, 0, 0, 0])
+		// state.inputs.ws.send(ss.buffer)
+		// console.log(ss.buffer) 
+		// setTimeout(() => {
+		// 	let ss = new Int8Array([102, 0, 4, 0, 0, 0, -112, 0, 0, 0])
+		// 	state.inputs.ws.send(ss.buffer)
+		// }, 100)
+		
+		
+		
+		
+		// console.log('!!!')
+		// let keydown = new KeyboardEvent('keydown',{code: "KeyQ", 'keyCode':81 , key: 'q'})
+		// var key = new Messages.SpiceMsgcKeyDown(keydown)
+		// var msg = new Messages.SpiceMiniData();
+		// // check_and_update_modifiers(e, key.code, state);
+		// msg.build_msg(Constants.SPICE_MSGC_INPUTS_KEY_DOWN, key); 
+		// console.log(msg)
+		// // console.log(msg, Constants)
+		// // state.inputs.send_msg(msg);
+		// // console.log(state.inputs.state)
+		// setTimeout(() => {
+			
+		// 	let keyup = new KeyboardEvent('keyup',{code: "KeyQ" , key: "q", keyCode: 81})
+		// 	var key = new Messages.SpiceMsgcKeyDown(keyup)
+		// 	var msg = new Messages.SpiceMiniData();
+		// 	// check_and_update_modifiers(e, key.code, state);
+		// 	msg.build_msg(Constants.SPICE_MSGC_INPUTS_KEY_UP, key);
+		// 	// state.inputs.send_msg(msg);
+		// }, 100);
+		// // check_and_update_modifiers(keydown, key.code, this.sc);
+		// console.log(msg)
 		
 		// console.log(msg)
 		// msg.build_msg(Constants.SPICE_MSGC_INPUTS_KEY_DOWN, key);
@@ -73,24 +106,30 @@
 	onMount(async () => {  
 		
 		const module = await import('/spice-html5/src/main.js'); 
-		Messages = await import('/spice-html5/src/spicemsg.js'); 
-		Constants = await import('/spice-html5/src/enums.js'); 
-		Constants = Constants.Constants
-
+		// Messages = await import('/spice-html5/src/spicemsg.js'); 
+		// Constants = await import('/spice-html5/src/enums.js'); 
+		SpiceConn = await import('/spice-html5/src/spiceconn.js'); 
+		SpiceConn = SpiceConn.SpiceConn
+		// Constants = Constants.Constants
+		
 		SpiceHtml5 = module;
 		let  {url,port,password,}= $page.query
-
+		
 		if(url.indexOf('ws') < 0){
 			url = 'ws://'+url;
 		}
 		if(password){ 
 			let addressurl = location.search.replace(`&password=${password}` , '')
-		
+			
 		}
 		state = new SpiceHtml5.SpiceMainConn({uri: url+':'+port, screen_id: "spice-screen", password: password });
-		 
 		
-		asdf();
+		// let test = {};
+		// let arg = [{chan_id: 0, type: 3, uri: "ws://khjde.xyz:5958/" , parent :state}]
+		// SpiceConn.apply(test, arg);
+		// let sss =  Object.create(SpiceConn.prototype);
+		
+		// asdf();
 
 	});
 </script>
